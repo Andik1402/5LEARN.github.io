ea6c39cb9bb54a0d55b376edcf0cd04de95b219e
Andik1402 <andika140202@gmai.com>
2023-01-17 06:46:20 +0700

    first upload

---
+++ js/script_question_begin_pracwriting.js
@@ -0,0 +1,119 @@
+//selecting all required elements
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+
+// if startQuiz button clicked
+start_btn.onclick = () => {
+    info_box.classList.add("activeInfo"); //show info box
+}
+
+// if exitQuiz button clicked
+exit_btn.onclick = () => {
+    info_box.classList.remove("activeInfo"); //hide info box
+}
+
+// if continueQuiz button clicked
+continue_btn.onclick = () => {
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.add("activeQuiz");
+    next_btn.classList.add("show");
+}
+
+var correct = 0;
+
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+// if restartQuiz button clicked
+restart_quiz.onclick = () => {
+    quiz_box.classList.add("activeQuiz"); //show quiz box
+    result_box.classList.remove("activeResult"); //hide result box
+    correct = 0;
+    next_btn.classList.add("show");
+    document.getElementById("quiz").reset();
+}
+
+// if quitQuiz button clicked
+quit_quiz.onclick = () => {
+    window.location.reload(); //reload the current window
+}
+
+const next_btn = document.querySelector("footer .next_btn");
+const bottom_ques_counter = document.querySelector("footer .total_que");
+
+// if Next Que button clicked
+next_btn.onclick = () => {
+    var question1 = document.quiz.question1.value;
+    var question2 = document.quiz.question2.value;
+    var question3 = document.quiz.question3.value;
+    var question4 = document.quiz.question4.value;
+    var question5 = document.quiz.question5.value;
+    var question6 = document.quiz.question6.value;
+    var question7 = document.quiz.question7.value;
+    var question8 = document.quiz.question8.value;
+    var question9 = document.quiz.question9.value;
+    var question10 = document.quiz.question10.value;
+
+
+    if (question1 == "in") {
+        correct++;
+    }
+    if (question2 == "dad's") {
+        correct++;
+    }
+    if (question3 == "a") {
+        correct++;
+    }
+    if (question4 == "is got") {
+        correct++;
+    }
+    if (question5 == "reading") {
+        correct++;
+    }
+    if (question6 == "than ") {
+        correct++;
+    }
+    if (question7 == "wloves") {
+        correct++;
+    }
+    if (question8 == "twice") {
+        correct++;
+    }
+    if (question9 == "his") {
+        correct++;
+    }
+    if (question10 == "taller") {
+        correct++;
+    }
+    console.log("You got " + correct + " correct.");
+    document.forms['quiz'].reset()
+    showResult();
+}
+
+
+
+
+function showResult() {
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.remove("activeQuiz"); //hide quiz box
+    result_box.classList.add("activeResult"); //show result box
+    const scoreText = result_box.querySelector(".score_text");
+    if (correct > 3) { // if user scored more than 3
+        //creating a new span tag and passing the user score number and total question number
+        let scoreTag = '<span>and congrats! 🎉, You got <p>' + correct + '</p> correct <p>' + '</p></span>';
+        scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
+    }
+    else if (correct > 1) { // if user scored more than 1
+        let scoreTag = '<span>and nice 😎, You got <p>' + correct + '</p> correct <p>'  + '</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else { // if user scored less than 1
+        let scoreTag = '<span>and sorry 😐, You got only <p>' + correct + '</p> correct <p>'  + '</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+}
+
